c8e5baa8da289765ad8ebc8684cda3d1d313727a
jinsoo <binary01@kakao.com>
2023-08-28 20:36:02 +0900

    feat: eslint prettier add

--- travel-app/.eslintrc.cjs
+++ travel-app/.eslintrc.cjs
@@ -5,14 +5,14 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'plugin:testing-library/react',
+    'plugin:jest-dom/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint', 'prettier', 'testing-library', 'jest-dom'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
-}
+};
