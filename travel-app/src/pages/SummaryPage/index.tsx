1212506f063b1c534151af3af5f665d9eb628d4b
jinsoo <binary01@kakao.com>
2023-08-28 21:06:28 +0900

    feat: tdd add

---
+++ travel-app/src/pages/SummaryPage/index.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+const SummaryPage = () => {
+  const [checked, setChecked] = useState(false);
+
+  return (
+    <div>
+      <form>
+        <input
+          id="confirm-checkbox"
+          type="checkbox"
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
+        />
+        <label htmlFor="confirm-checkbox">주문하려는 것을 확인하셨나요?</label>
+        <br />
+        <button
+          type="submit"
+          disabled={!checked}
+        >
+          주문 확인
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SummaryPage;
